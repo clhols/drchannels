c72712d4634642f57817a2f7c35203add3008f10
Claus Holst <clhols@gmail.com>
2018-05-17 21:25:30 +0200

    Fixes Lint checks

--- app/src/main/java/dk/youtec/drchannels/ui/view/AutoMaxLinesTextView.kt
+++ app/src/main/java/dk/youtec/drchannels/ui/view/AutoMaxLinesTextView.kt
@@ -1,12 +1,10 @@
 package dk.youtec.drchannels.ui.view
 
-import android.annotation.TargetApi
 import android.content.Context
-import android.os.Build
+import android.support.v7.widget.AppCompatTextView
 import android.util.AttributeSet
-import android.widget.TextView
 
-open class AutoMaxLinesTextView : TextView {
+open class AutoMaxLinesTextView : AppCompatTextView {
     protected var limitedMaxLines = Integer.MAX_VALUE
 
     constructor(context: Context) : super(context)
@@ -14,9 +12,6 @@ open class AutoMaxLinesTextView : TextView {
     constructor(context: Context, attrs: AttributeSet) : super(context, attrs)
 
     constructor(context: Context, attrs: AttributeSet, defStyleAttr: Int) : super(context, attrs, defStyleAttr)
-
-    @TargetApi(Build.VERSION_CODES.LOLLIPOP)
-    constructor(context: Context, attrs: AttributeSet, defStyleAttr: Int, defStyleRes: Int) : super(context, attrs, defStyleAttr, defStyleRes)
 
     override fun onMeasure(widthMeasureSpec: Int, heightMeasureSpec: Int) {
         val mode = MeasureSpec.getMode(heightMeasureSpec)
