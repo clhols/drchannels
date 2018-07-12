664025094f878edc9dd54ae04b46d8f7145ea95d
Claus Holst <clhols@gmail.com>
2018-07-12 18:28:53 +0200

    Trying to fix DrTvBootReceiver not executing

--- app/src/main/java/dk/youtec/drchannels/service/DrTvBootReceiver.kt
+++ app/src/main/java/dk/youtec/drchannels/service/DrTvBootReceiver.kt
@@ -6,6 +6,7 @@ import android.content.ComponentName
 import android.content.Context
 import android.content.Intent
 import android.os.Build
+import android.util.Log
 import androidx.core.content.systemService
 import com.google.android.media.tv.companionlibrary.EpgSyncJobService
 import java.util.concurrent.TimeUnit
@@ -13,37 +14,36 @@ import java.util.concurrent.TimeUnit
 class DrTvBootReceiver : BroadcastReceiver() {
 
     override fun onReceive(context: Context, intent: Intent?) {
-        if (intent?.action == Intent.ACTION_BOOT_COMPLETED) {
+        Log.v("DrTvBootReceiver", "onReceive")
 
-            val jobScheduler: JobScheduler = context.systemService()
+        val jobScheduler: JobScheduler = context.systemService()
 
-            // If there are not pending jobs. Create a sync job and schedule it.
-            val pendingJobs = jobScheduler.allPendingJobs
-            if (pendingJobs.isEmpty()) {
-                val inputId = context.getSharedPreferences(
-                        EpgSyncJobService.PREFERENCE_EPG_SYNC,
-                        Context.MODE_PRIVATE)
-                        .getString(EpgSyncJobService.BUNDLE_KEY_INPUT_ID, null)
+        // If there are not pending jobs. Create a sync job and schedule it.
+        val pendingJobs = jobScheduler.allPendingJobs
+        if (pendingJobs.isEmpty()) {
+            val inputId = context.getSharedPreferences(
+                    EpgSyncJobService.PREFERENCE_EPG_SYNC,
+                    Context.MODE_PRIVATE)
+                    .getString(EpgSyncJobService.BUNDLE_KEY_INPUT_ID, null)
 
-                if (inputId != null) {
-                    // Set up periodic sync only when input has set up.
-                    EpgSyncJobService.setUpPeriodicSync(
-                            context,
-                            inputId,
-                            ComponentName(context, DrTvEpgJobService::class.java),
-                            TimeUnit.HOURS.toMillis(12),
-                            TimeUnit.DAYS.toMillis(6))
-                }
-                return
+            if (inputId != null) {
+                // Set up periodic sync only when input has set up.
+                EpgSyncJobService.setUpPeriodicSync(
+                        context,
+                        inputId,
+                        ComponentName(context, DrTvEpgJobService::class.java),
+                        TimeUnit.HOURS.toMillis(12),
+                        TimeUnit.DAYS.toMillis(6))
             }
-            // On L/L-MR1, reschedule the pending jobs.
-            if (Build.VERSION.SDK_INT <= Build.VERSION_CODES.LOLLIPOP_MR1) {
-                pendingJobs
-                        .filter { it.isPersisted }
-                        .forEach { jobScheduler.schedule(it) }
-            }
-
-            schedulePreviewUpdate()
+            return
         }
+        // On L/L-MR1, reschedule the pending jobs.
+        if (Build.VERSION.SDK_INT <= Build.VERSION_CODES.LOLLIPOP_MR1) {
+            pendingJobs
+                    .filter { it.isPersisted }
+                    .forEach { jobScheduler.schedule(it) }
+        }
+
+        schedulePreviewUpdate()
     }
 }
