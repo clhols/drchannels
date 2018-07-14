0bb3c85dff88a4fbed18624f8b96e42bf1b372b7
Claus Holst <clhols@gmail.com>
2018-07-14 15:57:26 +0200

    TV check added

--- app/src/main/java/dk/youtec/drchannels/service/DrTvBootReceiver.kt
+++ app/src/main/java/dk/youtec/drchannels/service/DrTvBootReceiver.kt
@@ -9,6 +9,7 @@ import android.os.Build
 import android.util.Log
 import androidx.core.content.systemService
 import com.google.android.media.tv.companionlibrary.EpgSyncJobService
+import dk.youtec.drchannels.util.isTv
 import java.util.concurrent.TimeUnit
 
 class DrTvBootReceiver : BroadcastReceiver() {
@@ -16,30 +17,32 @@ class DrTvBootReceiver : BroadcastReceiver() {
     private val inputId = "dk.youtec.drchannels/.service.DrTvInputService"
 
     override fun onReceive(context: Context, intent: Intent?) {
-        val jobScheduler: JobScheduler = context.systemService()
+        if (context.isTv()) {
+            val jobScheduler: JobScheduler = context.systemService()
 
-        // If there are not pending jobs. Create a sync job and schedule it.
-        val pendingJobs = jobScheduler.allPendingJobs
-        if (pendingJobs.isEmpty()) {
-            Log.v(tag, "No pending jobs")
+            // If there are not pending jobs. Create a sync job and schedule it.
+            val pendingJobs = jobScheduler.allPendingJobs
+            if (pendingJobs.isEmpty()) {
+                Log.v(tag, "No pending jobs")
 
-            Log.v(tag, "Calling EpgSyncJobService.setUpPeriodicSync")
-            // Set up periodic sync only when input has set up.
-            EpgSyncJobService.setUpPeriodicSync(
-                    context,
-                    inputId,
-                    ComponentName(context, DrTvEpgJobService::class.java),
-                    TimeUnit.HOURS.toMillis(12),
-                    TimeUnit.DAYS.toMillis(6))
-        } else {
-            // On L/L-MR1, reschedule the pending jobs.
-            if (Build.VERSION.SDK_INT <= Build.VERSION_CODES.LOLLIPOP_MR1) {
-                pendingJobs
-                        .filter { it.isPersisted }
-                        .forEach { jobScheduler.schedule(it) }
+                Log.v(tag, "Calling EpgSyncJobService.setUpPeriodicSync")
+                // Set up periodic sync only when input has set up.
+                EpgSyncJobService.setUpPeriodicSync(
+                        context,
+                        inputId,
+                        ComponentName(context, DrTvEpgJobService::class.java),
+                        TimeUnit.HOURS.toMillis(12),
+                        TimeUnit.DAYS.toMillis(6))
+            } else {
+                // On L/L-MR1, reschedule the pending jobs.
+                if (Build.VERSION.SDK_INT <= Build.VERSION_CODES.LOLLIPOP_MR1) {
+                    pendingJobs
+                            .filter { it.isPersisted }
+                            .forEach { jobScheduler.schedule(it) }
+                }
             }
-        }
 
-        schedulePreviewUpdate()
+            schedulePreviewUpdate()
+        }
     }
 }
