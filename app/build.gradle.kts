1b94bff3856ae0f3afa29aeb0cae5c054ca78448
Claus Holst <clhols@gmail.com>
2018-07-18 21:28:58 +0200

    New coroutines version

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -76,8 +76,8 @@ dependencies {
     implementation(project(":drapi"))
     implementation(project(":tv-library"))
     implementation("org.jetbrains.kotlin:kotlin-stdlib-jdk8:$kotlinVersion")
-    implementation("org.jetbrains.kotlinx:kotlinx-coroutines-core:0.23.3")
-    implementation("org.jetbrains.kotlinx:kotlinx-coroutines-android:0.23.3")
+    implementation("org.jetbrains.kotlinx:kotlinx-coroutines-core:0.23.4")
+    implementation("org.jetbrains.kotlinx:kotlinx-coroutines-android:0.23.4")
 
     implementation("com.google.android.exoplayer:exoplayer:2.8.2")
 
