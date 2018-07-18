b5d9e08d8bef7cb28fb440fb7aa06f749f38cff6
Claus Holst <clhols@gmail.com>
2018-07-18 14:53:41 +0200

    Gradle 4.9 and okhttp 3.11.0

--- drapi/build.gradle.kts
+++ drapi/build.gradle.kts
@@ -6,7 +6,7 @@ plugins {
 
 dependencies {
     implementation(kotlin("stdlib"))
-    implementation("com.squareup.okhttp3:okhttp:3.10.0")
+    implementation("com.squareup.okhttp3:okhttp:3.11.0")
     implementation("com.squareup.retrofit2:retrofit:2.3.0")
     implementation("com.squareup.retrofit2:converter-jackson:2.3.0")
     implementation("com.fasterxml.jackson.module:jackson-module-kotlin:2.9.+")
