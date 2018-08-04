7312b84def74375e29934a28fb53ef147cca8f93
Claus Holst <clhols@gmail.com>
2018-08-04 12:37:14 +0200

    Added GlideModule

--- drapi/build.gradle.kts
+++ drapi/build.gradle.kts
@@ -7,9 +7,9 @@ plugins {
 dependencies {
     implementation(kotlin("stdlib"))
     implementation("com.squareup.okhttp3:okhttp:3.11.0")
-    implementation("com.squareup.retrofit2:retrofit:2.3.0")
+    implementation("com.squareup.retrofit2:retrofit:2.4.0")
     implementation("com.squareup.retrofit2:converter-jackson:2.3.0")
-    implementation("com.fasterxml.jackson.module:jackson-module-kotlin:2.9.+")
+    implementation("com.fasterxml.jackson.module:jackson-module-kotlin:2.9.6")
 
     testImplementation("junit:junit:4.12")
 }
