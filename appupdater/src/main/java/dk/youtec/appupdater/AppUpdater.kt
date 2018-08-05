8535433170b3c6fed8950ded5dfb637fe8d7c037
Claus Holst <clhols@gmail.com>
2018-08-05 16:47:47 +0200

    Added app updater dialog

--- appupdater/src/main/java/dk/youtec/appupdater/AppUpdater.kt
+++ appupdater/src/main/java/dk/youtec/appupdater/AppUpdater.kt
@@ -1,15 +1,21 @@
 package dk.youtec.appupdater
 
+import android.app.AlertDialog
 import android.content.Context
 import android.content.Intent
-import android.support.annotation.WorkerThread
 import android.support.v4.content.FileProvider
 import android.util.Log
+import kotlinx.coroutines.CommonPool
+import kotlinx.coroutines.android.UI
+import kotlinx.coroutines.launch
+import kotlinx.coroutines.withContext
 import okhttp3.CacheControl
 import okhttp3.Request
 import java.io.File
 import okio.Okio
 import org.json.JSONArray
+
+private const val tag = "AppUpdater"
 
 /**
  * Updates the app to a newer version.
@@ -17,62 +23,82 @@ import org.json.JSONArray
  * @param metaUrl Url pointing to the output.json file from the Gradle build.
  * @param apkUrl Url pointing to the APK file.
  */
-@WorkerThread
 fun updateApp(context: Context, versionCode: Int, metaUrl: String, apkUrl: String) {
-    val tag = "AppUpdater"
+    launch(UI) {
+        val metaAppVersion = getAppVersionFromMeta(context, metaUrl)
+        Log.v(tag, "Meta has version code $metaAppVersion")
 
-    val metaAppVersion = getAppVersionFromMeta(context, metaUrl)
-    Log.v(tag, "Meta has version code $metaAppVersion")
-
-    if (metaAppVersion > versionCode) {
-        val httpClient = OkHttpClientFactory.getInstance(context)
-
-        val request = Request.Builder()
-                .url(apkUrl)
-                .cacheControl(CacheControl.Builder().noStore().build())
-                .build()
-        val response = httpClient.newCall(request).execute()
-        val source = response.body()?.source()
-
-        if (source != null) {
-            val cacheDir = context.externalCacheDir ?: context.cacheDir
-            val apkCacheFolder = File(cacheDir, "apk").apply { mkdirs() }
-            val apkFile = File(apkCacheFolder, "app.apk").apply { createNewFile() }
-            Okio.buffer(Okio.sink(apkFile)).apply {
-                writeAll(source)
-                close()
-            }
-
-            Log.v(tag, "Downloaded APK to ${apkFile.absolutePath} with size ${apkFile.length()}")
-
-            try {
-                val authority = "${context.packageName}.fileprovider"
-                val uri = FileProvider.getUriForFile(context, authority, apkFile)
-                val type = "application/vnd.android.package-archive"
-
-                val intent = Intent(Intent.ACTION_VIEW).apply {
-                    setDataAndType(uri, type)
-                    flags = Intent.FLAG_ACTIVITY_NEW_TASK or Intent.FLAG_GRANT_READ_URI_PERMISSION
-                }
-
-                context.startActivity(intent)
-            } catch (e: Exception) {
-                Log.e(tag, e.message, e)
-            }
+        if (metaAppVersion > versionCode) {
+            AlertDialog.Builder(context)
+                    .setTitle(context.getString(R.string.updateApp))
+                    .setCancelable(true)
+                    .setMessage(context.getString(R.string.newAppVersionReady))
+                    .setPositiveButton(context.getString(R.string.update)) { _, _ ->
+                        launch(UI) {
+                            downloadApk(context, apkUrl)?.also {
+                                installApk(context, it)
+                            }
+                        }
+                    }
+                    .create().show()
+        } else {
+            Log.d(tag, "App is the latest version $versionCode")
         }
-    } else {
-        Log.d(tag, "App is the latest version $versionCode")
     }
 }
 
-@WorkerThread
-private fun getAppVersionFromMeta(context: Context, metaUrl: String): Int {
+private suspend fun downloadApk(context: Context, apkUrl: String): File? = withContext(CommonPool) {
+    val httpClient = OkHttpClientFactory.getInstance(context)
+
+    val request = Request.Builder()
+            .url(apkUrl)
+            .cacheControl(CacheControl.Builder().noStore().build())
+            .build()
+    val response = httpClient.newCall(request).execute()
+    val source = response.body()?.source()
+
+    if (source != null) {
+        val cacheDir = context.externalCacheDir ?: context.cacheDir
+        val apkCacheFolder = File(cacheDir, "apk").apply { mkdirs() }
+        val apkFile = File(apkCacheFolder, "app.apk").apply { createNewFile() }
+        Okio.buffer(Okio.sink(apkFile)).apply {
+            writeAll(source)
+            close()
+        }
+
+        Log.v(tag, "Downloaded APK to ${apkFile.absolutePath} with size ${apkFile.length()}")
+
+        apkFile
+    } else {
+        null
+    }
+}
+
+private fun installApk(context: Context, apkFile: File) {
+    try {
+        val authority = "${context.packageName}.fileprovider"
+        val uri = FileProvider.getUriForFile(context, authority, apkFile)
+        val type = "application/vnd.android.package-archive"
+
+        val intent = Intent(Intent.ACTION_VIEW).apply {
+            setDataAndType(uri, type)
+            flags = Intent.FLAG_ACTIVITY_NEW_TASK or Intent.FLAG_GRANT_READ_URI_PERMISSION
+        }
+
+        context.startActivity(intent)
+    } catch (e: Exception) {
+        Log.e(tag, e.message, e)
+    }
+}
+
+private suspend fun getAppVersionFromMeta(context: Context, metaUrl: String): Int = withContext(
+        CommonPool) {
     val httpClient = OkHttpClientFactory.getInstance(context)
 
     val request = Request.Builder().url(metaUrl).build()
     val response = httpClient.newCall(request).execute()
     val metaString = response.body()?.string() ?: ""
 
-    return JSONArray(metaString).optJSONObject(0).optJSONObject("apkInfo")?.optInt("versionCode")
+    JSONArray(metaString).optJSONObject(0).optJSONObject("apkInfo")?.optInt("versionCode")
             ?: BuildConfig.VERSION_CODE
 }
