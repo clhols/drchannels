245125e8681e25306ec1d85060c73a21f5bfa67e
Claus Holst <clhols@gmail.com>
2018-08-11 19:34:44 +0200

    Force same dependency versions

--- build.gradle.kts
+++ build.gradle.kts
@@ -27,6 +27,19 @@ allprojects {
         jcenter()
         maven { setUrl("http://dl.bintray.com/kotlin/kotlin-eap") }
     }
+
+    // Force all of the primary libraries to use the same version.
+    configurations.all {
+        resolutionStrategy {
+            eachDependency {
+                when (requested.group) {
+                    "com.android.support" -> useVersion(supportLibVersion)
+                    "android.arch.core" -> useVersion(archComponentVersion)
+                    "android.arch.lifecycle" -> useVersion(archComponentVersion)
+                }
+            }
+        }
+    }
 }
 
 val kotlinVersion by extra("1.3-M1")
@@ -34,4 +47,6 @@ val compileSdkVersion by extra(28)
 val targetSdkVersion by extra(28)
 val buildToolsVersion by extra("28.0.2")
 val supportLibVersion by extra("27.1.1")
+val archComponentVersion by extra("1.1.1")
 val glideVersion by extra("4.7.1")
+val isCiBuild = System.getenv("CI") == "true"
