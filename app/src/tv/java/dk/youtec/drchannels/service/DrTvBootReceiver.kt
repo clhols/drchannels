e05b915870f5eeaa84fd4d10f775b6e1e4711d84
Claus Holst <clhols@gmail.com>
2018-08-25 20:07:14 +0200

    Schedule current programs update after boot

--- app/src/tv/java/dk/youtec/drchannels/service/DrTvBootReceiver.kt
+++ app/src/tv/java/dk/youtec/drchannels/service/DrTvBootReceiver.kt
@@ -47,6 +47,8 @@ class DrTvBootReceiver : BroadcastReceiver() {
                             }
                 }
             }
+
+            scheduleCurrentProgramsPreviewUpdate()
         }
     }
 }
