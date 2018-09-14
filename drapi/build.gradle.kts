38d68ffa3c7a6b01d20ae758ca73bbe9f3d3f9a7
Claus Holst <clhols@gmail.com>
2018-09-14 17:40:15 +0200

    Jackson converter 2.4.0

--- drapi/build.gradle.kts
+++ drapi/build.gradle.kts
@@ -8,7 +8,7 @@ dependencies {
     implementation(kotlin("stdlib"))
     implementation("com.squareup.okhttp3:okhttp:3.11.0")
     implementation("com.squareup.retrofit2:retrofit:2.4.0")
-    implementation("com.squareup.retrofit2:converter-jackson:2.3.0")
+    implementation("com.squareup.retrofit2:converter-jackson:2.4.0")
     implementation("com.jakewharton.retrofit:retrofit2-kotlin-coroutines-adapter:0.9.1")
     implementation("com.fasterxml.jackson.module:jackson-module-kotlin:2.9.6")
 
