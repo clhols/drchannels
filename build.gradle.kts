543887673396e691243553e6508e8b82c5cc4a60
Claus Holst <clhols@gmail.com>
2018-09-20 20:33:55 +0200

    Remove comment

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,8 +1,6 @@
 import org.gradle.kotlin.dsl.extra
 import org.gradle.kotlin.dsl.repositories
 
-// Top-level build file where you can add configuration options common to all sub-projects/modules.
-
 buildscript {
     repositories {
         mavenCentral()
