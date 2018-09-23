e57ecb2e9b86bd558fec06631b487af071f4405e
Claus Holst <clhols@gmail.com>
2018-09-23 17:27:52 +0200

    New Kotlin and coroutines versions

--- build.gradle.kts
+++ build.gradle.kts
@@ -9,7 +9,7 @@ buildscript {
         maven { setUrl("http://dl.bintray.com/kotlin/kotlin-eap") }
     }
 
-    val kotlinVersion by extra("1.3-M2")
+    val kotlinVersion by extra("1.3+")
 
     dependencies {
         classpath("com.android.tools.build:gradle:3.3.0+")
@@ -34,18 +34,19 @@ allprojects {
                     "com.android.support" -> useVersion(supportLibVersion)
                     "android.arch.core" -> useVersion(archComponentVersion)
                     "android.arch.lifecycle" -> useVersion(archComponentVersion)
+                    "org.jetbrains.kotlin" -> useVersion(kotlinVersion)
                 }
             }
         }
     }
 }
 
-val kotlinVersion by extra("1.3-M2")
+val kotlinVersion by extra("1.3+")
 val compileSdkVersion by extra(28)
 val targetSdkVersion by extra(28)
 val buildToolsVersion by extra("28.0.2")
 val supportLibVersion by extra("27.1.1")
 val archComponentVersion by extra("1.1.1")
 val glideVersion by extra("4.8.0")
-val coroutinesVersion by extra("0.26.0-eap13")
+val coroutinesVersion by extra("0.26.1-eap13")
 val isCiBuild = System.getenv("CI") == "true"
