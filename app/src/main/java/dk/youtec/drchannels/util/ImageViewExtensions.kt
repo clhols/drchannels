9a358d46ea43376324886194d9fe2f6ddd3819f8
Claus Holst <clhols@gmail.com>
2018-10-31 20:26:40 +0100

    Replace Glide with Picasso

--- app/src/main/java/dk/youtec/drchannels/util/ImageViewExtensions.kt
+++ app/src/main/java/dk/youtec/drchannels/util/ImageViewExtensions.kt
@@ -2,11 +2,16 @@ package dk.youtec.drchannels.util
 
 import android.widget.ImageView
 import com.squareup.picasso.Picasso
+import com.squareup.picasso.RequestCreator
 import dk.youtec.drchannels.R
 
-fun ImageView.load(url: String) {
+fun ImageView.load(
+        url: String,
+        options: RequestCreator.() -> RequestCreator = { this }
+) {
     Picasso.get()
             .load(url)
+            .options()
             .placeholder(R.drawable.image_placeholder)
             .into(this)
 }
