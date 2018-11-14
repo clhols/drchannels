ce6bdce6476632ad8b84c629f15f7b2becb9ae9e
Claus Holst <clhols@gmail.com>
2018-11-14 19:41:46 +0100

    Set Picasso instance

--- app/src/main/java/dk/youtec/drchannels/DrApplication.kt
+++ app/src/main/java/dk/youtec/drchannels/DrApplication.kt
@@ -1,12 +1,20 @@
 package dk.youtec.drchannels
 
 import android.os.Looper
+import com.squareup.picasso.OkHttp3Downloader
+import com.squareup.picasso.Picasso
+import dk.youtec.drchannels.backend.OkHttpClientFactory
 import io.reactivex.android.plugins.RxAndroidPlugins
 import io.reactivex.android.schedulers.AndroidSchedulers
 
 class DrApplication : android.app.Application() {
     override fun onCreate() {
         super.onCreate()
+
+        Picasso.setSingletonInstance(
+                Picasso.Builder(this)
+                        .downloader(OkHttp3Downloader(OkHttpClientFactory.getInstance(this)))
+                        .build())
 
         RxAndroidPlugins.setInitMainThreadSchedulerHandler {
             AndroidSchedulers.from(Looper.getMainLooper(), true)
