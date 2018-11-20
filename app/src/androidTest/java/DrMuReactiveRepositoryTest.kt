154b3287b1baa350228e2b779c5329d8a7822f0e
Claus Holst <clhols@gmail.com>
2018-11-20 19:08:58 +0100

    Fix test

--- app/src/androidTest/java/DrMuReactiveRepositoryTest.kt
+++ app/src/androidTest/java/DrMuReactiveRepositoryTest.kt
@@ -1,6 +1,9 @@
-import androidx.test.platform.app.InstrumentationRegistry
+import androidx.test.core.app.ApplicationProvider
 import androidx.test.ext.junit.runners.AndroidJUnit4
+import dk.youtec.drapi.ProgramCard
+import dk.youtec.drchannels.DrApplication
 import dk.youtec.drchannels.backend.DrMuReactiveRepository
+import io.reactivex.observers.TestObserver
 import io.reactivex.rxkotlin.toObservable
 import org.junit.Test
 import org.junit.runner.RunWith
@@ -9,15 +12,21 @@ import org.junit.runner.RunWith
 open class DrMuReactiveRepositoryTest {
     @Test
     fun testSearch() {
-        val context = InstrumentationRegistry.getInstrumentation().context
+        val context: DrApplication = ApplicationProvider.getApplicationContext()
+        val testObserver = TestObserver<ProgramCard>()
 
-        DrMuReactiveRepository(context).search("Tæt på sandheden")
+        val title = "Tæt på sandheden"
+        DrMuReactiveRepository(context).search(title)
                 .toObservable()
                 .flatMap {
                     it.Items.toObservable()
                 }
-                .subscribe {
-                    System.out.println("Got ${it.Title}")
-                }
+                .subscribe(testObserver)
+
+        testObserver.awaitCount(1)
+        testObserver.assertComplete()
+        testObserver.assertValueAt(0) {
+            it.SeriesTitle == title
+        }
     }
 }
