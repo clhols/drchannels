5747386c14bde923d62a9dcd5b8cae6fa6f9011a
Claus Holst <clhols@gmail.com>
2018-11-30 19:28:31 +0100

    Serialization version

--- appupdater/build.gradle.kts
+++ appupdater/build.gradle.kts
@@ -28,7 +28,7 @@ dependencies {
     implementation("org.jetbrains.kotlin:kotlin-stdlib-jdk8:$kotlinVersion")
     implementation("org.jetbrains.kotlinx:kotlinx-coroutines-core:$coroutinesVersion")
     implementation("org.jetbrains.kotlinx:kotlinx-coroutines-android:$coroutinesVersion")
-    implementation("org.jetbrains.kotlinx:kotlinx-serialization-runtime:0.9.0")
+    implementation("org.jetbrains.kotlinx:kotlinx-serialization-runtime:$serializationVersion")
     implementation("androidx.appcompat:appcompat:$supportLibVersion")
     implementation("androidx.legacy:legacy-support-v4:$supportLibVersion")
     implementation("com.squareup.okhttp3:okhttp:$okhttpVersion")
