3076071d88025ca063b12066cf21b36314ff81de
Claus Holst <clh@visiolink.com>
2018-12-14 19:17:01 +0100

    Kotlin 1.3.11

--- buildSrc/build.gradle.kts
+++ buildSrc/build.gradle.kts
@@ -10,5 +10,5 @@ repositories {
 dependencies {
     compileOnly(gradleApi())
     implementation("com.android.tools.build:gradle:3.2.1")
-    implementation(kotlin("stdlib", "1.3.10"))
+    implementation(kotlin("stdlib", "1.3.11"))
 }
