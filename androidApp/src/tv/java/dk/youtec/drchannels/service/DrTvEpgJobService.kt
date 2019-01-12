34c99216b5f0094346c6dd052589408682d9ecff
Claus Holst <clh@visiolink.com>
2019-01-12 14:50:00 +0100

    Change back to 1280x720 to avoid UI showing Full HD

--- androidApp/src/tv/java/dk/youtec/drchannels/service/DrTvEpgJobService.kt
+++ androidApp/src/tv/java/dk/youtec/drchannels/service/DrTvEpgJobService.kt
@@ -141,8 +141,8 @@ class DrTvEpgJobService : EpgSyncJobService(), CoroutineScope {
             setPosterArtUri(broadcast.ProgramCard.PrimaryImageUri)
 
             if (broadcast.VideoHD && broadcast.VideoWidescreen) {
-                setVideoHeight(1080)
-                setVideoWidth(1920)
+                setVideoHeight(720)
+                setVideoWidth(1280)
             }
 
             val providerData = InternalProviderData().apply {
