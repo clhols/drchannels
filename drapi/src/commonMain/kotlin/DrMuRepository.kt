2f5ec20f3650b3b7f4d5fae70b8f190fabb79e85
Claus Holst <clh@visiolink.com>
2019-01-19 12:36:19 +0100

    Added update menu item and set JSON parsing to nonstrict

--- drapi/src/commonMain/kotlin/DrMuRepository.kt
+++ drapi/src/commonMain/kotlin/DrMuRepository.kt
@@ -9,48 +9,48 @@ open class DrMuRepository: IDrMuApi {
         val url = "$API_URL/channel/all-active-dr-tv-channels"
         val client = HttpClientFactory.create()
         val json = client.get<String>(url)
-        return JSON.parse(Channel.serializer().list, json)
+        return JSON.nonstrict.parse(Channel.serializer().list, json)
     }
 
     override suspend fun getManifest(uri: String): Manifest {
         val client = HttpClientFactory.create()
         val json = client.get<String>(uri)
-        return JSON.parse(Manifest.serializer(), json)
+        return JSON.nonstrict.parse(Manifest.serializer(), json)
     }
 
     override suspend fun getSchedule(id: String, date: String): Schedule {
         val url = "$API_URL/schedule/$id?broadcastdate=$date"
         val client = HttpClientFactory.create()
         val json = client.get<String>(url)
-        return JSON.parse(Schedule.serializer(), json)
+        return JSON.nonstrict.parse(Schedule.serializer(), json)
     }
 
     override suspend fun getScheduleNowNext(id: String): MuNowNext {
         val url = "$API_URL/schedule/nownext/$id"
         val client = HttpClientFactory.create()
         val json = client.get<String>(url)
-        return JSON.parse(MuNowNext.serializer(), json)
+        return JSON.nonstrict.parse(MuNowNext.serializer(), json)
     }
 
     override suspend fun getScheduleNowNext(): List<MuNowNext> {
         val url = "$API_URL/schedule/nownext-for-all-active-dr-tv-channels"
         val client = HttpClientFactory.create()
         val json = client.get<String>(url)
-        return JSON.parse(MuNowNext.serializer().list, json)
+        return JSON.nonstrict.parse(MuNowNext.serializer().list, json)
     }
 
     override suspend fun search(query: String): SearchResult {
         val url = "$API_URL/search/tv/programcards-with-asset/title/$query"
         val client = HttpClientFactory.create()
         val json = client.get<String>(url)
-        return JSON.parse(SearchResult.serializer(), json)
+        return JSON.nonstrict.parse(SearchResult.serializer(), json)
     }
 
     override suspend fun getMostViewed(channel: String, channelType: String, limit: Int): MostViewed {
         val url = "$API_URL/list/view/mostviewed?channel=$channel&channelType=$channelType&limit=$limit"
         val client = HttpClientFactory.create()
         val json = client.get<String>(url)
-        return JSON.parse(MostViewed.serializer(), json)
+        return JSON.nonstrict.parse(MostViewed.serializer(), json)
     }
 
     override suspend fun getPageTvPrograms(genre: Genre): Page {
