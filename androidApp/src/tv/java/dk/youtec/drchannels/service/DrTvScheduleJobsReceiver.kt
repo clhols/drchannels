68589e88ed8e07923135d78ae90bfe8d536de6c0
Claus Holst <clhols@gmail.com>
2019-01-26 21:02:41 +0100

    Enable genre preview channels

--- androidApp/src/tv/java/dk/youtec/drchannels/service/DrTvScheduleJobsReceiver.kt
+++ androidApp/src/tv/java/dk/youtec/drchannels/service/DrTvScheduleJobsReceiver.kt
@@ -8,15 +8,16 @@ import android.content.Context.JOB_SCHEDULER_SERVICE
 import android.content.Intent
 import android.os.Build
 import android.util.Log
+import androidx.annotation.RequiresApi
 import androidx.work.Data
 import com.google.android.media.tv.companionlibrary.EpgSyncJobService
 import dk.youtec.drapi.Genre
-import dk.youtec.drchannels.BuildConfig
 import dk.youtec.drchannels.R
 import dk.youtec.drchannels.preview.*
 import dk.youtec.drchannels.util.isTv
 import java.util.concurrent.TimeUnit
 
+@RequiresApi(Build.VERSION_CODES.LOLLIPOP)
 class DrTvScheduleJobsReceiver : BroadcastReceiver() {
     private val tag = DrTvScheduleJobsReceiver::class.java.simpleName
 
@@ -54,17 +55,15 @@ class DrTvScheduleJobsReceiver : BroadcastReceiver() {
 
             scheduleCurrentProgramsPreviewUpdate()
             schedulePreviewUpdate<MostViewedPreviewUpdater>()
-            if (BuildConfig.DEBUG) {
-                schedulePreviewUpdate<SearchPreviewUpdater>()
-                schedulePreviewUpdate<GenrePreviewUpdater>(with(Genre.Sport))
-                schedulePreviewUpdate<GenrePreviewUpdater>(with(Genre.NaturViden))
-                schedulePreviewUpdate<GenrePreviewUpdater>(with(Genre.NyhederAktualitet))
-                schedulePreviewUpdate<GenrePreviewUpdater>(with(Genre.Drama))
-                schedulePreviewUpdate<GenrePreviewUpdater>(with(Genre.Dokumentar))
-                schedulePreviewUpdate<GenrePreviewUpdater>(with(Genre.Kultur))
-                schedulePreviewUpdate<GenrePreviewUpdater>(with(Genre.Underholdning))
-                schedulePreviewUpdate<GenrePreviewUpdater>(with(Genre.Livsstil))
-            }
+            schedulePreviewUpdate<SearchPreviewUpdater>()
+            schedulePreviewUpdate<GenrePreviewUpdater>(with(Genre.Sport))
+            schedulePreviewUpdate<GenrePreviewUpdater>(with(Genre.NaturViden))
+            schedulePreviewUpdate<GenrePreviewUpdater>(with(Genre.NyhederAktualitet))
+            schedulePreviewUpdate<GenrePreviewUpdater>(with(Genre.Drama))
+            schedulePreviewUpdate<GenrePreviewUpdater>(with(Genre.Dokumentar))
+            schedulePreviewUpdate<GenrePreviewUpdater>(with(Genre.Kultur))
+            schedulePreviewUpdate<GenrePreviewUpdater>(with(Genre.Underholdning))
+            schedulePreviewUpdate<GenrePreviewUpdater>(with(Genre.Livsstil))
         }
     }
 }
