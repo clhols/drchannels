167463fbdc53ab43ba9150c93d9f3198223fef82
Claus Holst <clhols@gmail.com>
2019-02-14 21:16:57 +0100

    Fixed tests

--- drapi/src/commonTest/kotlin/DateSerializerTests.kt
+++ drapi/src/commonTest/kotlin/DateSerializerTests.kt
@@ -11,13 +11,13 @@ class DateSerializerTests {
     fun testDate1() {
         val df = DateFormat("yyyy-MM-dd'T'HH:mm:ss.SSS'Z'")
         val date = df.parse("2018-11-24T19:01:26.2Z").local
-        assertEquals(1543082486002L, date.unixMillisLong, "Unix millis wrong")
+        assertEquals(1543086086002L, date.unixMillisLong, "Unix millis wrong")
     }
 
     @Test
     fun testDate2() {
         val df = DateFormat("yyyy-MM-dd'T'HH:mm:ss'Z'")
         val date2 = df.parse("2018-12-23T22:59:00Z").local
-        assertEquals(1545602340000L, date2.unixMillisLong, "Unix millis wrong")
+        assertEquals(1545605940000L, date2.unixMillisLong, "Unix millis wrong")
     }
 }
