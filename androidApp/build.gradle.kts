b3e1293b44b82a80b535bf10ab711746c82ae9ef
Claus Holst <clhols@gmail.com>
2019-06-05 19:46:18 +0200

    New test lib versions

--- androidApp/build.gradle.kts
+++ androidApp/build.gradle.kts
@@ -133,14 +133,14 @@ dependencies {
     implementation("androidx.lifecycle:lifecycle-extensions:$archComponentVersion")
     kapt("androidx.lifecycle:lifecycle-common-java8:$archComponentVersion")
 
-    androidTestImplementation("androidx.test:core:1.1.0")
-    androidTestImplementation("androidx.test:core-ktx:1.1.0")
+    androidTestImplementation("androidx.test:core:1.2.0")
+    androidTestImplementation("androidx.test:core-ktx:1.2.0")
     androidTestImplementation("androidx.test:runner:1.2.0")
-    androidTestImplementation("androidx.test:rules:1.1.1")
-    androidTestImplementation("androidx.test.ext:junit:1.1.0")
-    androidTestImplementation("androidx.test.ext:junit-ktx:1.1.0")
-    androidTestImplementation("androidx.test.espresso:espresso-core:3.2.0+")
-    androidTestUtil("androidx.test:orchestrator:1.1.1")
+    androidTestImplementation("androidx.test:rules:1.2.0")
+    androidTestImplementation("androidx.test.ext:junit:1.1.1")
+    androidTestImplementation("androidx.test.ext:junit-ktx:1.1.1")
+    androidTestImplementation("androidx.test.espresso:espresso-core:3.2.0")
+    androidTestUtil("androidx.test:orchestrator:1.2.0")
 }
 
 play {
