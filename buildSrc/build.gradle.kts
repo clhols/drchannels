6e6608cab22fa2f383a10749378ef3ba0aa168e3
Claus Holst <clhols@gmail.com>
2019-06-21 20:36:12 +0200

    AGP 3.6

--- buildSrc/build.gradle.kts
+++ buildSrc/build.gradle.kts
@@ -5,10 +5,11 @@ plugins {
 repositories {
     google()
     mavenCentral()
+    jcenter()
 }
 
 dependencies {
     compileOnly(gradleApi())
-    implementation("com.android.tools.build:gradle:3.4.1")
+    implementation("com.android.tools.build:gradle:3.6+")
     implementation(kotlin("stdlib", "1.3.40"))
 }
