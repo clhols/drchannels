e685cdb1aeda31a221afb36cba7effbfc2cf7eb0
Claus Holst <clhols@gmail.com>
2019-06-26 19:52:38 +0200

    Revert to 3.4.1 to BasePlugin cast issue

--- buildSrc/build.gradle.kts
+++ buildSrc/build.gradle.kts
@@ -10,6 +10,6 @@ repositories {
 
 dependencies {
     compileOnly(gradleApi())
-    implementation("com.android.tools.build:gradle:3.6+")
+    implementation("com.android.tools.build:gradle:3.4.1")
     implementation(kotlin("stdlib", "1.3.40"))
 }
