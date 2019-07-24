77bb57b26449172d274285aef08363b82f9e7f64
Claus Holst <clhols@gmail.com>
2019-07-24 20:50:04 +0200

    Const depends

--- buildSrc/src/main/java/dk/youtec/drchannels/Depends.kt
+++ buildSrc/src/main/java/dk/youtec/drchannels/Depends.kt
@@ -1,17 +1,16 @@
 //Project dependency versions
-val androidGradlePlugin = "3.6+"
-val kotlinVersion = "1.3.41"
-val compileSdk = 29
-val targetSdk = 29
-val buildTools = "29.0.1"
-val androidxVersion = "1.0.0"
-val appcompatVersion = "1.1.0-rc01"
-val archComponentVersion = "2.2.0+"
-val coroutinesVersion = "1.3.0-RC"
-val serializationVersion = "0.11.1"
-val okhttpVersion = "4.0.0"
-val junitVersion = "5.4.2"
 val isCiBuild = System.getenv("CI") == "true"
-val exoPlayerVersion = "2.10.1"
-val klockVersion = "1.5.0"
-val ktorVersion = "1.2.2"
+const val androidGradlePlugin = "3.6+"
+const val kotlinVersion = "1.3.41"
+const val compileSdk = 29
+const val targetSdk = 29
+const val buildTools = "29.0.1"
+const val androidxVersion = "1.0.0"
+const val appcompatVersion = "1.1.0-rc01"
+const val archComponentVersion = "2.2.0+"
+const val coroutinesVersion = "1.3.0-RC"
+const val serializationVersion = "0.11.1"
+const val okhttpVersion = "4.0.0"
+const val exoPlayerVersion = "2.10.1"
+const val klockVersion = "1.5.0"
+const val ktorVersion = "1.2.2"
