b2ebd8fbf8883ef3ef651224f091e02915b5a7a5
Claus Holst <clhols@gmail.com>
2019-08-09 23:04:43 +0200

    Fix tests

--- drapi/src/androidTest/kotlin/DrMuRepoTests.kt
+++ drapi/src/androidTest/kotlin/DrMuRepoTests.kt
@@ -14,7 +14,7 @@ class DrMuRepoTests {
     fun testAllActiveDrTvChannels() = runBlocking {
         val channels: List<Channel> = service.getAllActiveDrTvChannels()
 
-        val channelIds = channels.map { it.Slug }
+        val channelIds = channels.map { it.slug }
         val expectedChannelIds = listOf("dr1", "dr2", "dr3", "dr-k", "dr-ramasjang", "dr-ultra")
 
         assertTrue(
@@ -34,20 +34,20 @@ class DrMuRepoTests {
         val date = SimpleDateFormat("yyyy-MM-dd HH:mm:ss").format(Date())
         val schedule = service.getSchedule("dr1", date)
 
-        assertEquals("dr1", schedule.ChannelSlug)
+        assertEquals("dr1", schedule.channelSlug)
     }
 
     @Test
     fun testSearch() = runBlocking {
         val searchResult = service.search("bonderøven")
 
-        assertEquals(searchResult.Items.first().SeriesSlug, "bonderoeven-tv")
+        assertEquals(searchResult.items.first().seriesSlug, "bonderoeven-tv")
     }
 
     @Test
     fun testMostViewed() = runBlocking {
         val searchResult = service.getMostViewed("", "TV", 10)
 
-        assertEquals(searchResult.Items.size, 10)
+        assertEquals(searchResult.items.size, 10)
     }
 }
