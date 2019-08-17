1d5b767a0c384aa6b422ffcd4b58895ab09fcbd8
Claus Holst <clhols@gmail.com>
2019-08-17 21:48:39 +0200

    Use class delegation to delegate to common view model

--- androidApp/src/main/java/dk/youtec/drchannels/viewmodel/TvChannelsViewModel.kt
+++ androidApp/src/main/java/dk/youtec/drchannels/viewmodel/TvChannelsViewModel.kt
@@ -2,22 +2,8 @@ package dk.youtec.drchannels.viewmodel
 
 import androidx.annotation.Keep
 import androidx.lifecycle.ViewModel
-import androidx.lifecycle.viewModelScope
-import dk.youtec.drapi.MuNowNext
 import dk.youtec.drchannels.logic.viewmodel.TvChannelsViewModel
 import dk.youtec.drchannels.logic.viewmodel.TvChannelsViewModelImpl
-import kotlinx.coroutines.flow.Flow
 
 @Keep
-class TvChannelsViewModel : ViewModel(), TvChannelsViewModel {
-    private val viewModel = TvChannelsViewModelImpl()
-
-    override val channels: Flow<List<MuNowNext>> = viewModel.channels
-    override val playbackUri: Flow<String> = viewModel.playbackUri
-    override val error: Flow<String> = viewModel.error
-
-    override fun playTvChannel(muNowNext: MuNowNext) = viewModel.playTvChannel(muNowNext)
-    override fun playProgram(muNowNext: MuNowNext) = viewModel.playProgram(muNowNext)
-    override fun reload() = viewModel.reload()
-    override fun onCleared() = viewModel.onCleared()
-}
+class TvChannelsViewModel : ViewModel(), TvChannelsViewModel by TvChannelsViewModelImpl()
