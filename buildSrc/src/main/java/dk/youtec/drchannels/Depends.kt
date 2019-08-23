2a62c7a927b917e47c339dacc923cc305c56fd04
Claus Holst <clhols@gmail.com>
2019-08-23 22:01:14 +0200

    Coroutines 1.3.0

--- buildSrc/src/main/java/dk/youtec/drchannels/Depends.kt
+++ buildSrc/src/main/java/dk/youtec/drchannels/Depends.kt
@@ -2,13 +2,13 @@
 val isCiBuild = System.getenv("CI") == "true"
 const val androidGradlePlugin = "3.5.0"
 const val kotlinVersion = "1.3.50"
+const val coroutinesVersion = "1.3.0"
 const val compileSdk = 29
 const val targetSdk = 29
 const val buildTools = "29.0.2"
 const val androidxVersion = "1.0.0"
 const val appcompatVersion = "1.1.0-rc01"
 const val archComponentVersion = "2.2.0+"
-const val coroutinesVersion = "1.3.0-RC2"
 const val serializationVersion = "0.11.1"
 const val okhttpVersion = "4.1.0"
 const val exoPlayerVersion = "2.10.3"
