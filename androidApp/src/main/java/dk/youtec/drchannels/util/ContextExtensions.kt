f17b8959d7c3ca05e565ee206a7e5672a95f1ec9
Claus Holst <clhols@gmail.com>
2019-10-23 23:44:04 +0200

    Remove AnkoExtensions

--- androidApp/src/main/java/dk/youtec/drchannels/util/ContextExtensions.kt
+++ androidApp/src/main/java/dk/youtec/drchannels/util/ContextExtensions.kt
@@ -2,5 +2,26 @@ package dk.youtec.drchannels.util
 
 import android.content.Context
 import android.content.pm.PackageManager
+import android.widget.Toast
+import androidx.annotation.StringRes
+import androidx.preference.PreferenceManager
 
 fun Context.isTv(): Boolean = packageManager.hasSystemFeature(PackageManager.FEATURE_LEANBACK)
+
+fun Context.toast(@StringRes message: Int): Toast = Toast
+        .makeText(this, message, Toast.LENGTH_SHORT)
+        .apply {
+            show()
+        }
+
+fun Context.toast(message: CharSequence): Toast = Toast
+        .makeText(this, message, Toast.LENGTH_SHORT)
+        .apply {
+            show()
+        }
+
+inline val Context.displayMetrics: android.util.DisplayMetrics
+    get() = resources.displayMetrics
+
+inline val Context.defaultSharedPreferences: android.content.SharedPreferences
+    get() = PreferenceManager.getDefaultSharedPreferences(this)
