a724443fd1641e68a312237acab51e96573d4eb5
Claus Holst <clhols@gmail.com>
2019-11-25 21:06:43 +0100

    Kotlin 1.3.60 and libs

--- buildSrc/src/main/kotlin/dk/youtec/drchannels/Depends.kt
+++ buildSrc/src/main/kotlin/dk/youtec/drchannels/Depends.kt
@@ -1,7 +1,5 @@
 //Project dependency versions
 const val androidGradlePlugin = "3.6.0-beta04"
-const val kotlinVersion = "1.3.50"
-const val coroutinesVersion = "1.3.2"
 const val compileSdk = 29
 const val targetSdk = 29
 const val minSdk = 21
@@ -9,8 +7,12 @@ const val buildTools = "29.0.2"
 const val appcompatVersion = "1.1.0"
 const val androidAnnotation = "1.1.0"
 const val archComponentVersion = "2.2.0-beta01"
-const val serializationVersion = "0.13.0"
 const val okhttpVersion = "4.2.2"
 const val exoPlayerVersion = "2.10.5"
-const val klockVersion = "1.7.5"
-const val ktorVersion = "1.3.0-beta-1"
+
+//Kotlin dependent versions
+const val kotlinVersion = "1.3.60"
+const val coroutinesVersion = "1.3.2-1.3.60"
+const val serializationVersion = "0.14.0"
+const val klockVersion = "1.8.0"
+const val ktorVersion = "1.3.0-beta-2"
