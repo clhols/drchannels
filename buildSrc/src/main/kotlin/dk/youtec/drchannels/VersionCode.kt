8ab7b0514815cef42b5871188448025efe575263
Claus Holst <clhols@gmail.com>
2020-03-31 18:50:34 +0200

    New versions

--- buildSrc/src/main/kotlin/dk/youtec/drchannels/VersionCode.kt
+++
@@ -1,7 +0,0 @@
-import java.text.SimpleDateFormat
-import java.util.*
-
-val versionCodeTimestamp
-    get() = SimpleDateFormat("yyMMddHH00", Locale.US)
-            .apply { timeZone = TimeZone.getTimeZone("Europe/Copenhagen") }
-            .format(Date()).toInt()
