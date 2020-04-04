4553125d4ee2cc4280d980b0d92f73ae98340218
Claus Holst <clhols@gmail.com>
2020-04-04 20:12:24 +0200

    Revert to coroutines 1.3.5 as ktor doesn't like native-mt

--- buildSrc/src/main/kotlin/dk/youtec/drchannels/Versions.kt
+++ buildSrc/src/main/kotlin/dk/youtec/drchannels/Versions.kt
@@ -10,7 +10,7 @@ const val exoPlayerVersion = "2.11.3"
 
 //Kotlin dependent versions
 const val kotlinVersion = "1.3.71"
-const val coroutinesVersion = "1.3.5-native-mt"
+const val coroutinesVersion = "1.3.5"
 const val serializationVersion = "0.20.0"
 const val klockVersion = "1.9.1"
 const val ktorVersion = "1.3.2"
