d08f3e2e2d4e3423f43764ddc731f3baf5c7d34b
Claus Holst <clhols@gmail.com>
2020-04-05 19:28:37 +0200

    Set top padding from insets

--- androidApp/src/main/java/dk/youtec/drchannels/ui/ChannelsFragment.kt
+++ androidApp/src/main/java/dk/youtec/drchannels/ui/ChannelsFragment.kt
@@ -6,7 +6,9 @@ import android.os.Bundle
 import android.util.Log
 import android.view.*
 import androidx.core.net.toUri
+import androidx.core.view.ViewCompat
 import androidx.core.view.isVisible
+import androidx.core.view.updatePadding
 import androidx.fragment.app.Fragment
 import androidx.fragment.app.viewModels
 import androidx.lifecycle.lifecycleScope
@@ -36,6 +38,21 @@ class ChannelsFragment : Fragment() {
             container: ViewGroup?,
             savedInstanceState: Bundle?
     ): View = inflater.inflate(R.layout.fragment_channels, container, false)
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+
+        ViewCompat.setOnApplyWindowInsetsListener(view) { view, insets ->
+            view.updatePadding(top = insets.systemWindowInsetTop)
+
+            insets.replaceSystemWindowInsets(
+                    insets.systemWindowInsetLeft,
+                    0,
+                    insets.systemWindowInsetRight,
+                    insets.systemWindowInsetBottom
+            )
+        }
+    }
 
     override fun onActivityCreated(savedInstanceState: Bundle?) {
         super.onActivityCreated(savedInstanceState)
