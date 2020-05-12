e89bf2ddb302762509293baa0966d0069b02b140
Claus Holst <clhols@gmail.com>
2020-05-12 20:50:04 +0200

    New Firebase versions

--- androidApp/build.gradle.kts
+++ androidApp/build.gradle.kts
@@ -148,7 +148,7 @@ dependencies {
     implementation("androidx.work:work-runtime-ktx:2.3.4")
     implementation("androidx.lifecycle:lifecycle-runtime-ktx:2.2.0")
     implementation("androidx.lifecycle:lifecycle-livedata-ktx:2.2.0")
-    implementation(platform("com.google.firebase:firebase-bom:25.3.0"))
+    implementation(platform("com.google.firebase:firebase-bom:25.3.1"))
     implementation("com.google.firebase:firebase-analytics")
     implementation("com.google.firebase:firebase-perf")
     implementation("com.crashlytics.sdk.android:crashlytics")
