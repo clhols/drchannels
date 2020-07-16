a003448947dbd829f14160c340680113cf8e4da3
Claus Holst <clhols@gmail.com>
2020-07-17 00:10:18 +0200

    Update Versions.kt

--- buildSrc/src/main/kotlin/Versions.kt
+++ buildSrc/src/main/kotlin/Versions.kt
@@ -11,7 +11,7 @@ object Versions {
 
     //Kotlin dependent versions
     const val kotlin = "1.3.72"
-    const val coroutines = "1.3.7"
+    const val coroutines = "1.3.8"
     const val serialization = "0.20.0"
     const val ktor = "1.3.2"
 }
