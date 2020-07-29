667860db6774be949559f5c5624a874b2dff01fa
Claus Holst <clhols@gmail.com>
2020-07-30 00:05:11 +0200

    Target Android 11 (API 30) and build tools 30.0.1

--- buildSrc/src/main/kotlin/Versions.kt
+++ buildSrc/src/main/kotlin/Versions.kt
@@ -1,10 +1,10 @@
 object Versions {
     //Project dependency versions
     const val androidGradlePlugin = "4.0.0"
-    const val compileSdk = 29
-    const val targetSdk = 29
+    const val compileSdk = 30
+    const val targetSdk = 30
     const val minSdk = 21
-    const val buildTools = "29.0.3"
+    const val buildTools = "30.0.1"
     const val androidAnnotation = "1.1.0"
     const val okhttp = "4.7.2"
     const val exoPlayer = "2.11.7"
