6ae613918ac4adeb06a479890da7898eb3175706
Claus Holst <clhols@gmail.com>
2020-08-22 23:33:14 +0200

    Remove duplicate coroutines-core

--- logic/build.gradle.kts
+++ logic/build.gradle.kts
@@ -71,9 +71,7 @@ kotlin {
         }
 
         val iosMain by getting {
-            dependencies {
-                implementation("org.jetbrains.kotlinx:kotlinx-coroutines-core:${Versions.coroutines}")
-            }
+            dependencies {}
         }
         val iosTest by getting {
             dependencies {}
