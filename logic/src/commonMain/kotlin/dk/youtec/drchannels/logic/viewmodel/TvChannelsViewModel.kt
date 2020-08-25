a32ab4f81f3ddb2c1af2be9f9de1d78d57cdf5dc
Claus Holst <clhols@gmail.com>
2020-08-25 20:19:46 +0200

    Use extension property and cast to avoid private MutableStateFlow in view model

--- logic/src/commonMain/kotlin/dk/youtec/drchannels/logic/viewmodel/TvChannelsViewModel.kt
+++ logic/src/commonMain/kotlin/dk/youtec/drchannels/logic/viewmodel/TvChannelsViewModel.kt
@@ -2,6 +2,7 @@ package dk.youtec.drchannels.logic.viewmodel
 
 import dk.youtec.drapi.MuNowNext
 import kotlinx.coroutines.flow.Flow
+import kotlinx.coroutines.flow.MutableStateFlow
 import kotlinx.coroutines.flow.StateFlow
 
 interface TvChannelsViewModel {
@@ -12,4 +13,14 @@ interface TvChannelsViewModel {
     fun playTvChannel(muNowNext: MuNowNext)
     fun playProgram(muNowNext: MuNowNext)
     fun onCleared()
+
+    var <T> StateFlow<T>.state
+        get() = value
+        set(value: T) {
+            if (this is MutableStateFlow<T>) {
+                this.value = value
+            } else {
+                throw IllegalStateException("Not an instance of MutableStateFlow")
+            }
+        }
 }
