50aebc5254a74c350b15b9c40a726a36c8a5dcf0
Claus Holst <clhols@gmail.com>
2020-08-28 21:38:24 +0200

    Remove layout stable and full screen

--- androidApp/src/main/java/dk/youtec/drchannels/ui/MainActivity.kt
+++ androidApp/src/main/java/dk/youtec/drchannels/ui/MainActivity.kt
@@ -11,8 +11,6 @@ open class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
 
-        window.decorView.systemUiVisibility = View.SYSTEM_UI_FLAG_LAYOUT_STABLE or View.SYSTEM_UI_FLAG_LAYOUT_FULLSCREEN
-
         setContentView(R.layout.activity_main)
 
         if (savedInstanceState == null) {
