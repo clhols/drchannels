48fa694a180106a0470689b658137ecdefdf6a8f
Claus Holst <clhols@gmail.com>
2020-08-28 21:40:16 +0200

    Move adding fragment to XML

--- androidApp/src/main/java/dk/youtec/drchannels/ui/MainActivity.kt
+++ androidApp/src/main/java/dk/youtec/drchannels/ui/MainActivity.kt
@@ -1,9 +1,7 @@
 package dk.youtec.drchannels.ui
 
 import android.os.Bundle
-import android.view.View
 import androidx.appcompat.app.AppCompatActivity
-import androidx.fragment.app.commit
 import dk.youtec.drchannels.R
 
 open class MainActivity : AppCompatActivity() {
@@ -12,11 +10,5 @@ open class MainActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
 
         setContentView(R.layout.activity_main)
-
-        if (savedInstanceState == null) {
-            supportFragmentManager.commit {
-                replace(R.id.fragment, ChannelsFragment(), ChannelsFragment.TAG)
-            }
-        }
     }
 }
