c766124d83f39cdec0250f6ee84d4f08ea3d6ab0
Claus Holst <clhols@gmail.com>
2020-09-22 20:21:28 +0200

    Coil 1.0.0-rc3

--- androidApp/build.gradle.kts
+++ androidApp/build.gradle.kts
@@ -119,7 +119,7 @@ dependencies {
     implementation("com.google.android.exoplayer:exoplayer-ui:${Versions.exoPlayer}")
     implementation("com.google.android.exoplayer:extension-cast:${Versions.exoPlayer}")
 
-    implementation("io.coil-kt:coil:1.0.0-rc2")
+    implementation("io.coil-kt:coil:1.0.0-rc3")
     implementation("com.squareup.okhttp3:okhttp:${Versions.okhttp}")
     implementation("com.google.android.material:material:1.2.1")
     implementation("androidx.swiperefreshlayout:swiperefreshlayout:1.1.0")
