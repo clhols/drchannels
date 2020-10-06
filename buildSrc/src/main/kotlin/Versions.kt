66c7e36202fc8c1db698272de5a59e2fcb922d46
Claus Holst <clhols@gmail.com>
2020-10-06 22:15:18 +0200

    AGP 4.0.2

--- buildSrc/src/main/kotlin/Versions.kt
+++ buildSrc/src/main/kotlin/Versions.kt
@@ -1,6 +1,6 @@
 object Versions {
     //Project dependency versions
-    const val androidGradlePlugin = "4.0.1"
+    const val androidGradlePlugin = "4.0.2"
     const val compileSdk = 30
     const val targetSdk = 30
     const val minSdk = 21
