838ba3363ff494683a3af2eee77500c6d5ef00ae
Claus Holst <clhols@gmail.com>
2020-11-25 20:21:45 +0100

    Revert to Kotlin 1.4.10. New firebase versions AGP 4.1.1 Serialization 1.0.1

--- buildSrc/src/main/kotlin/Versions.kt
+++ buildSrc/src/main/kotlin/Versions.kt
@@ -1,6 +1,6 @@
 object Versions {
     //Project dependency versions
-    const val androidGradlePlugin = "4.1.0"
+    const val androidGradlePlugin = "4.1.1"
     const val compileSdk = 30
     const val targetSdk = 30
     const val minSdk = 21
@@ -11,8 +11,8 @@ object Versions {
     const val fragmentKtx = "1.2.5"
 
     //Kotlin dependent versions
-    const val kotlin = "1.4.20"
+    const val kotlin = "1.4.10"
     const val coroutines = "1.4.1-native-mt"
-    const val serialization = "1.0.0"
+    const val serialization = "1.0.1"
     const val ktor = "1.4.2"
 }
