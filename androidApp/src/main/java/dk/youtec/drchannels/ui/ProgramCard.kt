00d20558bf76bf6876b7bc2679c74f8fd4e1b598
Claus Holst <clhols@gmail.com>
2020-12-16 21:48:07 +0100

    Refactor

---
+++ androidApp/src/main/java/dk/youtec/drchannels/ui/ProgramCard.kt
@@ -0,0 +1,106 @@
+package dk.youtec.drchannels.ui
+
+import android.content.Context
+import androidx.compose.foundation.clickable
+import androidx.compose.foundation.layout.*
+import androidx.compose.foundation.shape.RoundedCornerShape
+import androidx.compose.material.Card
+import androidx.compose.material.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.platform.AmbientContext
+import androidx.compose.ui.text.TextStyle
+import androidx.compose.ui.text.font.FontFamily
+import androidx.compose.ui.text.font.FontWeight
+import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
+import coil.request.ImageRequest
+import coil.transform.RoundedCornersTransformation
+import dev.chrisbanes.accompanist.coil.CoilImage
+
+@Composable
+fun ProgramCard(
+        program: ProgramCardData,
+        onClick: (String) -> Unit
+) {
+    val context: Context = AmbientContext.current
+
+    Card(
+            Modifier.fillMaxWidth()
+                    .padding(4.dp)
+                    .clickable(onClick = { onClick(program.id) }),
+            shape = RoundedCornerShape(4.dp),
+    ) {
+        Column(Modifier.padding(12.dp)) {
+            Row {
+                CoilImage(
+                        request = ImageRequest.Builder(context)
+                                .data(program.imageUrl)
+                                .transformations(RoundedCornersTransformation(
+                                        topLeft = 40f,
+                                        bottomRight = 40f
+                                ))
+                                .build(),
+                        modifier = Modifier
+                                .preferredWidth(120.dp)
+                                .preferredHeight(80.dp)
+                )
+                Column {
+                    Text(
+                            text = program.title,
+                            modifier = Modifier.padding(start = 16.dp),
+                            style = TextStyle(
+                                    fontFamily = FontFamily.SansSerif,
+                                    fontWeight = FontWeight.Normal,
+                                    fontSize = 22.sp,
+                            )
+                    )
+                    Text(
+                            text = program.time,
+                            modifier = Modifier.padding(start = 16.dp),
+                            style = TextStyle(
+                                    fontFamily = FontFamily.SansSerif,
+                                    fontWeight = FontWeight.Normal,
+                                    fontSize = 18.sp,
+                            )
+                    )
+                }
+            }
+            if (program.description.isNotBlank()) {
+                Text(
+                        text = program.description,
+                        modifier = Modifier.padding(top = 16.dp, bottom = 8.dp),
+                        style = TextStyle(
+                                fontFamily = FontFamily.SansSerif,
+                                fontWeight = FontWeight.Light,
+                                fontSize = 16.sp,
+                        )
+                )
+            }
+        }
+    }
+}
+
+data class ProgramCardData(
+        val id: String,
+        val title: String,
+        val description: String,
+        val time: String,
+        val imageUrl: String
+)
+
+@Preview
+@Composable
+private fun PreviewProgramCard() {
+    ProgramCard(
+            program = ProgramCardData(
+                    "id",
+                    "Some program title",
+                    "Some not too long description",
+                    "12:00 ‣ 13:00",
+                    "https://asset.dr.dk/ImageScaler/?file=/mu-online/api/1.4/asset/5f394ae171401441844c2e2c%2525253Fraw=True&w=940&h=529&scaleAfter=crop&quality=75"
+            ),
+            onClick = {}
+    )
+}
--- androidApp/src/main/java/dk/youtec/drchannels/ui/ProgramsList.kt
+++ androidApp/src/main/java/dk/youtec/drchannels/ui/ProgramsList.kt
@@ -1,23 +1,7 @@
 package dk.youtec.drchannels.ui
 
-import android.content.Context
-import androidx.compose.foundation.clickable
-import androidx.compose.foundation.layout.*
 import androidx.compose.foundation.lazy.LazyColumnFor
-import androidx.compose.foundation.shape.RoundedCornerShape
-import androidx.compose.material.*
 import androidx.compose.runtime.Composable
-import androidx.compose.ui.Modifier
-import androidx.compose.ui.platform.AmbientContext
-import androidx.compose.ui.text.TextStyle
-import androidx.compose.ui.text.font.FontFamily
-import androidx.compose.ui.text.font.FontWeight
-import androidx.compose.ui.unit.dp
-import androidx.compose.ui.unit.sp
-import coil.request.ImageRequest
-import coil.transform.RoundedCornersTransformation
-import dev.chrisbanes.accompanist.coil.CoilImage
-import androidx.compose.ui.tooling.preview.Preview
 import dev.chrisbanes.accompanist.insets.AmbientWindowInsets
 import dev.chrisbanes.accompanist.insets.toPaddingValues
 import dk.youtec.drapi.MuScheduleBroadcast
@@ -70,90 +54,4 @@ fun getTime(dateTime: LocalDateTime): String {
         dateTime.minute
     }
     return "$hour:$minute"
-}
-
-data class ProgramCardData(
-        val id: String,
-        val title: String,
-        val description: String,
-        val time: String,
-        val imageUrl: String
-)
-
-@Composable
-private fun ProgramCard(
-        program: ProgramCardData,
-        onClick: (String) -> Unit
-) {
-    val context: Context = AmbientContext.current
-
-    Card(
-            Modifier.fillMaxWidth()
-                    .padding(4.dp)
-                    .clickable(onClick = { onClick(program.id) }),
-            shape = RoundedCornerShape(4.dp),
-    ) {
-        Column(Modifier.padding(12.dp)) {
-            Row {
-                CoilImage(
-                        request = ImageRequest.Builder(context)
-                                .data(program.imageUrl)
-                                .transformations(RoundedCornersTransformation(
-                                        topLeft = 40f,
-                                        bottomRight = 40f
-                                ))
-                                .build(),
-                        modifier = Modifier
-                                .preferredWidth(120.dp)
-                                .preferredHeight(80.dp)
-                )
-                Column {
-                    Text(
-                            text = program.title,
-                            modifier = Modifier.padding(start = 16.dp),
-                            style = TextStyle(
-                                    fontFamily = FontFamily.SansSerif,
-                                    fontWeight = FontWeight.Normal,
-                                    fontSize = 22.sp,
-                            )
-                    )
-                    Text(
-                            text = program.time,
-                            modifier = Modifier.padding(start = 16.dp),
-                            style = TextStyle(
-                                    fontFamily = FontFamily.SansSerif,
-                                    fontWeight = FontWeight.Normal,
-                                    fontSize = 18.sp,
-                            )
-                    )
-                }
-            }
-            if (program.description.isNotBlank()) {
-                Text(
-                        text = program.description,
-                        modifier = Modifier.padding(top = 16.dp, bottom = 8.dp),
-                        style = TextStyle(
-                                fontFamily = FontFamily.SansSerif,
-                                fontWeight = FontWeight.Light,
-                                fontSize = 16.sp,
-                        )
-                )
-            }
-        }
-    }
-}
-
-@Preview
-@Composable
-private fun PreviewChannelCard() {
-    ProgramCard(
-            program = ProgramCardData(
-                    "id",
-                    "Some program title",
-                    "Some not too long description",
-                    "12:00 ‣ 13:00",
-                    "https://asset.dr.dk/ImageScaler/?file=/mu-online/api/1.4/asset/5f394ae171401441844c2e2c%2525253Fraw=True&w=940&h=529&scaleAfter=crop&quality=75"
-            ),
-            onClick = {}
-    )
 }
