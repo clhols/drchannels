7e2ef9b70368a26079ca9f35eff1bd015fedf58a
Claus Holst <clhols@gmail.com>
2021-01-28 20:41:01 +0100

    Ktor 1.5.1

--- buildSrc/src/main/kotlin/Versions.kt
+++ buildSrc/src/main/kotlin/Versions.kt
@@ -16,5 +16,5 @@ object Versions {
     const val kotlin = "1.4.21"
     const val coroutines = "1.4.2-native-mt"
     const val serialization = "1.0.1"
-    const val ktor = "1.5.0"
+    const val ktor = "1.5.1"
 }
