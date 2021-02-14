13a02142d2c18fa309c311f115c3c64441414e40
Claus Holst <clhols@gmail.com>
2021-02-14 20:43:38 +0100

    Crashlytics 2.5.0

--- build.gradle.kts
+++ build.gradle.kts
@@ -18,7 +18,7 @@ buildscript {
         classpath("org.jetbrains.kotlin:kotlin-serialization:${Versions.kotlin}")
         classpath("com.google.gms:google-services:4.3.5")
         classpath("com.google.firebase:perf-plugin:1.3.4")
-        classpath("com.google.firebase:firebase-crashlytics-gradle:2.4.1")
+        classpath("com.google.firebase:firebase-crashlytics-gradle:2.5.0")
         classpath("dk.youtec:appupdater-plugin:1.1.0")
         classpath("org.jetbrains.compose:compose-gradle-plugin:+")
     }
