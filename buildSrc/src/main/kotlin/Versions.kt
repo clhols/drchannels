042dfef79c7b7137e47a296fd9fd1652195cdd67
Claus Holst <clhols@gmail.com>
2021-02-20 10:51:02 +0100

    Serialization 1.1.0

--- buildSrc/src/main/kotlin/Versions.kt
+++ buildSrc/src/main/kotlin/Versions.kt
@@ -15,6 +15,6 @@ object Versions {
     //Kotlin dependent versions
     const val kotlin = "1.4.30"
     const val coroutines = "1.4.2-native-mt"
-    const val serialization = "1.1.0-RC"
+    const val serialization = "1.1.0"
     const val ktor = "1.5.1"
 }
