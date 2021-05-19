243de96dfc86cb8d23e0d22f89c3825e98a409a8
Claus Holst <clhols@gmail.com>
2021-05-19 20:46:41 +0200

    AGP 7.0.0-beta01

--- buildSrc/src/main/kotlin/Versions.kt
+++ buildSrc/src/main/kotlin/Versions.kt
@@ -1,7 +1,7 @@
 @Suppress("unused")
 object Versions {
     //Project dependency versions
-    const val androidGradlePlugin = "7.0.0-alpha15"
+    const val androidGradlePlugin = "7.0.0-beta01"
     const val compileSdk = 30
     const val targetSdk = 30
     const val minSdk = 21
