8b0d071bbbf2c0295ab1b4c7a369c39e686862df
Claus Holst <clhols@gmail.com>
2021-07-13 22:16:22 +0200

    Ui-tooling 1.0.0-rc01

--- buildSrc/src/main/kotlin/Versions.kt
+++ buildSrc/src/main/kotlin/Versions.kt
@@ -10,7 +10,7 @@ object Versions {
     const val okhttp = "4.9.1"
     const val exoPlayer = "2.11.8"
     const val fragmentKtx = "1.2.5"
-    const val compose = "1.0.0-beta08"
+    const val compose = "1.0.0-rc01" //Only ui-tooling
 
     //Kotlin dependent versions
     const val kotlin = "1.5.10"
